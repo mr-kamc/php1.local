e7899a0f71b909a6faaa060bbe34bc3bd41b4542
mrkam <mr.kamc@gmail.com>
2018-10-31 14:58:17 +0300

    create models method insert

--- App/Model.php
+++ App/Model.php
@@ -34,4 +34,26 @@ abstract class Model
         return $res;
     }
 
+    public function insert()
+    {
+        $fields = get_object_vars($this);
+
+        $cols = [];
+        $data = [];
+
+        foreach ($fields as $name=>$value){
+            if ('id' == $name){
+                continue;
+            }
+            $cols[] = $name;
+            $data[':' . $name] = $value;
+        }
+        $sql = 'INSERT INTO ' . static::TABLE . ' (' . implode(',', $cols) . ')
+        VALUES 
+        (' . implode(',', array_keys($data)) . ')';
+
+        $db = new Db();
+        $db->execute($sql, $data);
+    }
+
 }
--- index.php
+++ index.php
@@ -1,13 +1,18 @@
 <?php
 
 require __DIR__ . '/autoload.php';
-
+/*
 $news = \App\Models\Article::findLast(3);
 include __DIR__ . '/templates/news/index.php';
+*/
+
+$article = new \App\Models\Article();
+$article->title = 'Название новости';
+$article->content = 'Текст новости';
+$article->insert();
 
 
 /*
-$data = \App\Models\Article::findAll();
-
-var_dump($data);
+$config = new \App\Config();
+var_dump($config->data['db']['host']);
 */
